02c7d0863e2f62c0f4d046dac11c95db0641cfd6
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-11-07 00:27:13 +0200

    add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+
 function App() {
-  return <div>Pizza App</div>;
+  return (
+    <>
+      <div>Pizza App</div>
+      <RegisterPage />
+    </>
+  );
 }
 export default App;
---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <div className="container">
+      <h1 className="page-title">Register page</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
