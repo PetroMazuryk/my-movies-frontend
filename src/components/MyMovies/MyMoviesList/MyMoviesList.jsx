4b53bbc5667b3f00cabdd2b99a9ddaf9a8926794
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-12-12 17:59:53 +0200

    fix MyMoviesList

--- src/components/MyMovies/MyMoviesList/MyMoviesList.jsx
+++ src/components/MyMovies/MyMoviesList/MyMoviesList.jsx
@@ -6,20 +6,31 @@ const VITE_API_URL = import.meta.env.VITE_API_URL;
 const MyMoviesList = ({ items, onDeleteMovie }) => {
   const imageRef = useRef(null);
 
-  const elements = items.map(({ _id, title, director, genre, poster }) => (
-    <li className={styles.listItem} key={_id}>
-      {poster && (
-        <img
-          ref={imageRef}
-          className={styles.listItemPoster}
-          src={`${VITE_API_URL}/${poster}`}
-          alt={`Poster for ${title}`}
-        />
-      )}
-      Title: {title}. Director: {director}. Genre: {genre}
-      <button onClick={() => onDeleteMovie(_id)}>Delete</button>
-    </li>
-  ));
+  const elements = items.map(
+    ({ _id, title, director, genre, poster, releaseDate }) => (
+      <li className={styles.listItem} key={_id}>
+        {poster && (
+          <div className={styles.posterWrapper}>
+            <img
+              ref={imageRef}
+              className={styles.listItemPoster}
+              src={`${VITE_API_URL}/${poster}`}
+              alt={`Poster for ${title}`}
+            />
+          </div>
+        )}
+        <div className={styles.listItemInfo}>
+          <h2>Назва: {title}</h2>
+          <span>Director: {director}</span>
+          <span>Genre: {genre}</span>
+          <span>Release: {releaseDate}</span>
+        </div>
+        <button className={styles.button} onClick={() => onDeleteMovie(_id)}>
+          Delete
+        </button>
+      </li>
+    )
+  );
 
   return <ol className={styles.list}>{elements}</ol>;
 };
