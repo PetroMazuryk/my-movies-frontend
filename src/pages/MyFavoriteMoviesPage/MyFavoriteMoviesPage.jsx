41ae36e0b1d8c6d29bee4cebda24895a88c97ed8
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-12-12 19:48:57 +0200

    add display elements to MyFaviriteMoviesPage

--- src/pages/MyFavoriteMoviesPage/MyFavoriteMoviesPage.jsx
+++ src/pages/MyFavoriteMoviesPage/MyFavoriteMoviesPage.jsx
@@ -1,21 +1,53 @@
+import { useRef } from 'react';
 import { useSelector } from 'react-redux';
-
+import { useDispatch } from 'react-redux';
 import { getFavoriteMovies } from '../../redux/movies/movies-selectors';
+import { deleteMovie } from '../../redux/movies/movies-operations';
+
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+import styles from './MyFavoriteMoviesPage.module.css';
+import MyBooksBlock from '../../components/MyMovies/MyMoviesBlock/MyMoviesBlock';
 
 const MyFavoriteMoviesPage = () => {
+  const imageRef = useRef(null);
+  const dispatch = useDispatch();
+
   const favoriteMovies = useSelector(getFavoriteMovies);
 
-  const elements = favoriteMovies.map(({ _id, title, director }) => (
-    <li key={_id}>
-      Title: {title}. Director: {director}.
-    </li>
-  ));
+  const onDeleteMovie = (id) => {
+    dispatch(deleteMovie(id));
+  };
+
+  const elements = favoriteMovies.map(
+    ({ _id, title, director, genre, poster, releaseDate }) => (
+      <li className={styles.listItem} key={_id}>
+        {poster && (
+          <div className={styles.posterWrapper}>
+            <img
+              ref={imageRef}
+              className={styles.listItemPoster}
+              src={`${VITE_API_URL}/${poster}`}
+              alt={`Poster for ${title}`}
+            />
+          </div>
+        )}
+        <div className={styles.listItemInfo}>
+          <h2>Назва: {title}</h2>
+          <span>Director: {director}</span>
+          <span>Genre: {genre}</span>
+          <span>Release: {releaseDate}</span>
+        </div>
+        <button className={styles.button} onClick={() => onDeleteMovie(_id)}>
+          Delete
+        </button>
+      </li>
+    )
+  );
 
   return (
-    <div className="container">
-      <h1 className="page-title">My favorite movies page</h1>
-      <ol>{elements}</ol>
-    </div>
+    <MyBooksBlock title="My favorite movies page">
+      <ol className={styles.list}>{elements}</ol>;
+    </MyBooksBlock>
   );
 };
 
