c1333f6808da90869389d640dd43dc904afe7fbd
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-12-14 16:22:55 +0000

    Deploying to gh-pages from @ PetroMazuryk/my-movies-frontend@5cbdd46986d602e495216ab22fbb36019bb180e3 🚀

---
+++ assets/NotFoundPage-74568430.js
@@ -0,0 +1,1 @@
+import{j as t,N as e}from"./index-244c16b2.js";const a=()=>t.jsx(e,{to:"/login",replace:!0});export{a as default};
--- assets/NotFoundPage-fecea557.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./index-f225107f.js";const a=()=>t.jsx(e,{to:"/login",replace:!0});export{a as default};
