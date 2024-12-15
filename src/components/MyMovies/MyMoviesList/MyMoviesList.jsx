242c8b62b50cd41b0834e3029d29f70990b2f95b
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-12-15 21:10:59 +0200

    add icon favorite movies

--- src/components/MyMovies/MyMoviesList/MyMoviesList.jsx
+++ src/components/MyMovies/MyMoviesList/MyMoviesList.jsx
@@ -1,11 +1,24 @@
 import { useRef } from 'react';
 import styles from './MyMoviesList.module.css';
 import EditMovie from '../EditMovie/EditMovie';
+import icon from '../../../assets/sprite.svg';
+import { useSelector } from 'react-redux';
+import { getFavoriteMovies } from '../../../redux/movies/movies-selectors';
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const MyMoviesList = ({ items, onDeleteMovie }) => {
   const imageRef = useRef(null);
+
+  const favoriteMovies = useSelector(getFavoriteMovies);
+  const isFovoriteMovies = favoriteMovies.some((_id) => _id === items._id);
+  const toggleFavoriteMovie = () => {
+    if (!isFovoriteMovies) {
+      //  dispatch(addMovieFavorite(item));
+    } else {
+      //  dispatch(deleteMovieFavorite(_id));
+    }
+  };
 
   const elements = items.map(
     ({ _id, title, director, genre, poster, releaseDate, favorite }) => (
@@ -26,6 +39,16 @@ const MyMoviesList = ({ items, onDeleteMovie }) => {
           <span>Genre: {genre}</span>
           <span>Release: {releaseDate}</span>
         </div>
+        <button>
+          <svg
+            onClick={toggleFavoriteMovie}
+            className={`${styles.like} ${
+              isFovoriteMovies ? styles.likeActive : ''
+            }`}
+          >
+            <use href={`${icon}#icon-heart`}></use>
+          </svg>
+        </button>
         <button className={styles.button} onClick={() => onDeleteMovie(_id)}>
           Delete
         </button>
